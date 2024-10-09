0aa273fcbd9acd701c3d8e151cd11500278951f5
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-10-09 13:57:11 +0100

    Update analysis.jl

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -342,36 +342,9 @@ datamodel1 = diffindiffparameters_splinetimes(
     psiprior=0.4,
 )
 
-datac1config = @ntuple modelname="datamodel1" model=datamodel1 n_rounds n_chains=8 seed=3010+id
+datac1config = @ntuple modelname="datamodel1" model=datamodel1 n_rounds n_chains=8 seed=1010+id
 datachain1dict = produce_or_load(pol_fitparameter, datac1config, datadir("sims"))
-#=
-dtimes = let 
-    timeperiods = ones(Int, 216)
-    for i ∈ 1:216 
-        timeperiods[i] = 1 + round(Int, (i - 1) / 43.2, RoundDown)
-    end
-    timeperiods
-end
 
-datamodel1discrete = diffindiffparameters_discretetimes(
-    W_allcoviddata, 
-    allcovidcases,
-    masstesting, 
-    dtimes,
-    selectpops;
-    psiprior=0.4,
-)
-
-datac1configdiscrete = (
-    modelname="datamodel1discrete",
-    model=datamodel1discrete,
-    n_rounds=n_rounds,
-    n_chains=8,
-    seed=515+id
-)
-datachain1dictdiscrete = produce_or_load(
-    pol_fitparameter, datac1configdiscrete, datadir("sims")
-)
 
 ## Analysis 2 
 # Pillar 1 test results 
@@ -385,89 +358,15 @@ datamodel2 = diffindiffparameters_splinetimes(
     psiprior=0.4,
 )
 
-datac2config = @ntuple modelname="datamodel2" model=datamodel2 n_rounds n_chains=8 seed=520+id
+datac2config = @ntuple modelname="datamodel2" model=datamodel2 n_rounds n_chains=8 seed=1020+id
 datachain2dict = produce_or_load(pol_fitparameter, datac2config, datadir("sims"))
-#=
-datamodel2discrete = diffindiffparameters_discretetimes(
-    W_pil1coviddata, 
-    pil1covidcases,
-    masstesting, 
-    dtimes,
-    selectpops;
-    psiprior=0.4,
-)
 
-datac2configdiscrete = (
-    modelname="datamodel2discrete",
-    model=datamodel2discrete,
-    n_rounds=n_rounds,
-    n_chains=8,
-    seed=525+id
-)
-datachain2dictdiscrete = produce_or_load(
-    pol_fitparameter, datac2configdiscrete, datadir("sims")
-)
-=#
 
-#=
-## Simulation 3 -- make 3 January (universal testing) a competing event for Liverpool 
+## Analysis 3
 
-# Limit to those local authorities near Liverpool that were in the same tiers for control in
-# 2020
-
-## Convert DataFrame to appropriate matrices 
-let 
-    # check that each location has the same number of rows 
-    for i ∈ 2:39 
-        @assert sum(coviddf2.location .== 1) == sum(coviddf2.location .== i) 
-    end
-    
-    # how many rows is it?
-    covidlength = sum(coviddf2.location .== 1)
-    #371
-
-    # limit analysis to those local authorities that were in the same tiers 
-    # (Halton, Knowsley, Liverpool, Sefton, St Helens, Wirral)
-    global allcovidcases2 = Matrix{Int}(undef, covidlength, 6)
-    global pil1covidcases2 = Matrix{Int}(undef, covidlength, 6)
-    #start date in Liverpool 7 November 2020 
-    stl = Dates.value(Date("2020-11-07") - Date("2020-05-31"))
-    # Liverpool stopped being unique 3 December 
-    str = Dates.value(Date("2020-12-03") - Date("2020-05-31"))
-    stv = [ i == 3 ? stl : str for i ∈ 1:6 ]
-    global masstesting2 = InterventionsMatrix(stv, covidlength)
-    global universaltesting = InterventionsMatrix(stv2, covidlength)
-    for i ∈ 1:6
-        k = [ 15, 17, 19,28, 31, 38 ][i]
-        _tdf = filter(:location => x -> x == k, coviddf2)
-        for j ∈ 1:covidlength
-            allcovidcases2[j, i] = _tdf.cases[j]
-            pil1covidcases2[j, i] = _tdf.pillar1cases[j]
-        end
-    end 
-end
-selectpops = [ populations[x] for x ∈ [ 15, 17, 19, 28, 31, 38 ] ]
-
-W_allcoviddata2 = generatew_gt(COVIDSERIALINTERVAL, allcovidcases2, selectpops)
-
+# with lead and lag 
 
 datamodel3 = diffindiffparameters_splinetimes(
-    W_allcoviddata2, 
-    allcovidcases2,
-    masstesting2, 
-    collect(1.0:28:371),
-    selectpops;
-    psiprior=0.4,
-    secondaryinterventions=universaltesting,
-)
-
-datac3config = @ntuple modelname="datamodel3" model=datamodel3 n_rounds n_chains=8 seed=530+id
-datachain3dict = produce_or_load(pol_fitparameter, datac3config, datadir("sims"))
-=#
-## with lead and lag 
-
-=#
-datamodel5 = diffindiffparameters_splinetimes(
     W_allcoviddata, 
     allcovidcases,
     masstesting, 
@@ -480,5 +379,5 @@ datamodel5 = diffindiffparameters_splinetimes(
     ]
 )
 
-datac5config = @ntuple modelname="datamodel5" model=datamodel5 n_rounds n_chains=8 seed=540+id
-datachain5dict = produce_or_load(pol_fitparameter, datac5config, datadir("sims"))
+datac3config = @ntuple modelname="datamodel3" model=datamodel3 n_rounds n_chains=8 seed=1030+id
+datachain3dict = produce_or_load(pol_fitparameter, datac3config, datadir("sims"))
