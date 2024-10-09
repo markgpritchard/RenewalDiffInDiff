788c0a7157b21d69faabde38ce9072b0fd8d80eb
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-10-09 13:57:57 +0100

    Create analysis_develsingle.sh

---
+++ scripts/analysis_develsingle.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=00:10:00
+#SBATCH --partition=devel
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mark.pritchard@ndm.ox.ac.uk
+
+export JULIA_NUM_THREADS=8
+module load Julia/1.9.3-linux-x86_64
+
+n_rounds=4
+n=1
+
+julia scripts/analysis.jl "$n" "$n_rounds" &
+
+wait
+
